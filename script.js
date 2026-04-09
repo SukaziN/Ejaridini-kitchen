ee8211cbb6af4f13ae6d63a405c90496f7daf310
SukaziN <nkosiyazisukazie@gmail.com>
2026-04-09 14:01:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function displayFeedback() {
+  let date = new Date();
+  console.log(date);
+  //formatDate; //make it so the day, date and time appear without the GMT stuff. Also make sure that once a comment has been left it keeps the time it was commented and doesn't refresh to the current date and time
+  let name = document.getElementById("name").value;
+  let surname = document.getElementById("surname").value;
+  let feedback = document.getElementById("comment").value;
+  let customerFeedback = document.getElementById("customer-feedback");
+  customerFeedback.classList.add("comment-box");
+  customerFeedback.innerText =
+    date + " " + feedback + " - " + name + " " + surname;
+}
