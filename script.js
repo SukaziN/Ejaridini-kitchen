7e63bc00b1b45a8402ee5a4903ddc421b949656e
SukaziN <nkosiyazisukazie@gmail.com>
2026-04-20 12:17:22 +0200

    added notes on embedding user feedback and analytics

--- script.js
+++ script.js
@@ -13,4 +13,25 @@ function displayFeedback() {
     customerFeedback.innerText = feedback + " - " + name + " " + surname;
   }
   alert("Thank you for your feedback!💖");
+
+  /* window.onload = function () {
+    let savedText = this.localStorage.getItem("storedText");
+    if (savedText) {
+      feedback.value = savedText;
+    }
+  };
+  feedback.addEventListener("input", function () {
+    localStorage.setItem("storedText", feedback.value);
+  });*/ //somehow the error is on line 25... figure out alternative way
 }
+
+/* <script src="https://cdn.jsdelivr.net/npm/counterapi/dist/counter.browser.min.js"></script> put this line in the head in html
+  <div>Views: <span id="counter">Loading...</span></div> put this line below header in html
+const counter = new counter({ workspace: "Ejaridini Kitchen" });
+counter
+  .up("page-views")
+  .then((result) => {
+    console.log(`Page views: ${result.value}`);
+    document.getElementById("counter").textContent = result.value;
+  })
+  .catch((error) => console.error("Counter error:", error.message));*/ //initiate the api key and api url first...
