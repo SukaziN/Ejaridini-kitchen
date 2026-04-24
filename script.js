4832d18d1a6ef22884ce87e7c5f3bdbcb33f40e6
SukaziN <nkosiyazisukazie@gmail.com>
2026-04-24 10:20:22 +0200

    commented out JS future update code for better performance

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let customerFeedback1 = document.getElementById("feedback-1");
-let customerFeedback2 = document.getElementById("feedback-2"); //these selectors are here as an effort to avoid using a loop when checking if the first li for feedback already has text so the comment can be displayed in one of them (for site performance concerns)
+/*let customerFeedback2 = document.getElementById("feedback-2"); //these selectors are here as an effort to avoid using a loop when checking if the first li for feedback already has text so the comment can be displayed in one of them (for site performance concerns)
 let customerFeedback3 = document.getElementById("feedback-3");
-let customerFeedback4 = document.getElementById("feedback-4");
+let customerFeedback4 = document.getElementById("feedback-4");*/
 function displayFeedback() {
   let name = document.getElementById("name").value;
   let surname = document.getElementById("surname").value;
@@ -12,12 +12,12 @@ function displayFeedback() {
     let message = feedback + " - " + "Anonymous";
     customerFeedback1.innerText = message;
     customerFeedback1 = message;
-    localStorage.setItem("key", message);
+    /*localStorage.setItem("key", message);
 
     if (localStorage.getItem("key")) {
       message = localStorage.getItem("key");
       console.log(message);
-    }
+    }*/
   } else {
     let message = feedback + " - " + name + " " + surname;
     customerFeedback1.innerText = message;
@@ -34,33 +34,33 @@ function displayFeedback() {
       let message = feedback + " - " + "Anonymous";
       customerFeedback1.innerText = message;
       customerFeedback1 = message;
-      localStorage.setItem("key", message);
+      /* localStorage.setItem("key", message);
 
       if (localStorage.getItem("key")) {
         message = localStorage.getItem("key");
         console.log(message);
-      }
+      }*/
     } else {
       let message = feedback + " - " + name + " " + surname;
       customerFeedback1.innerText = message;
       customerFeedback1 = message;
-      localStorage.setItem("key", message);
+      /* localStorage.setItem("key", message);
 
       if (localStorage.getItem("key")) {
         message = localStorage.getItem("key");
         console.log(message);
-      }
+      }*/
     }
   }
   alert("Thank you for your feedback!💖");
 }
-let mealMenu = document.getElementById("meal-menu");
-let platterMenu = document.getElementById("platter-menu");
+/*let mealMenu = document.getElementById("meal-menu");
+let platterMenu = document.getElementById("platter-menu");*/
 
-function makeMealMenuBig() {
-  platterMenu.classList.remove("make-bigger");
-  mealMenu.classList.add("make-bigger");
-}
+/*function makeMealMenuBig() {
+ // platterMenu.classList.remove("make-bigger");
+ // mealMenu.classList.add("make-bigger");
+//}
 function makePlatterMenuBig() {
   mealMenu.classList.remove("make-bigger");
   platterMenu.classList.add("make-bigger");
@@ -85,4 +85,4 @@ function tracker(e) {
     console.log(counter);
     console.log(counter.length); //this is how many times the ul in navigation is clicked
   }
-}
+}*/
